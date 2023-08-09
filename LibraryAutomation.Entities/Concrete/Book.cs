881cb4d24406c41f59edc981ba92852ea9822f12
tunahan özkan <mhnds_tunahan@hotmail.com>
2023-08-09 16:59:34 +0300

    Add project files.

---
+++ LibraryAutomation.Entities/Concrete/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryAutomation.Entities.Concrete
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public int BookName { get; set; }
+    }
+}
